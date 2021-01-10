9a7d95d6becabc2913584ffbed5695674da34434
Justin Ross <jross@apache.org>
2021-01-10 09:30:11 -0500

    WIP

--- config/config.py
+++ config/config.py
@@ -1,5 +1,11 @@
 site_url = ""
 
+def path_nav(page):
+    separator = "<span class=\"path-separator\">›</span>"
+    links = separator.join(list(page.path_nav_links)[1:])
+
+    return f"<nav class=\"path-nav\">{links}</nav>"
+
 def cell(column_index, value):
     if column_index == 0:
         value = f"<a href=\"/networks/company-co/index.html\">{value}</a>"
@@ -7,11 +13,19 @@ def cell(column_index, value):
     return f"<td>{value}</td>"
 
 headings = "Name", "Status", "Linked sites", "Created"
-network_table = html_table_csv("data/networks.csv", headings=headings, cell_fn=cell)
+
+data = (
+    ("company-co", "Active", "na-east, na-west, and headquarters", "4 hours ago"),
+    ("salty-pug", "Active", "store-1, factory-1, and 4 more", "3 days ago"),
+    ("bookinfo", "Disabled", "productpage, details, and 2 more", "1/8/2020"),
+)
+
+network_table = html_table(data, headings=headings, cell_fn=cell)
 
 props = (
     ("Name", "company-co"),
     ("Status", "Active"),
+    ("Linked sites", "na-east, na-west, and headquarters"),
     ("Created", "4 hours ago"),
 )
 
@@ -19,12 +33,20 @@ network_properties = html_table(props, class_="properties")
 
 def cell(column_index, value):
     if column_index == 0:
-        value = f"<a href=\"/networks/company-co/sites/na-east/index.html\">{value}</a>"
+        value = f"<a href=\"/networks/company-co/links/na-east-d45e/index.html\">{value}</a>"
 
     return f"<td>{value}</td>"
 
-headings = "Name", "Type", "Status"
-site_table = html_table_csv("data/sites.csv", headings=headings, cell_fn=cell)
+headings = "Name", "Status", "Site", "Cost", "Created"
+
+data = (
+    ("na-east-d45e", "OK", "na-east (OpenShift)", 1, "3 minutes ago"),
+    ("na-east-7c7b", "OK", "na-east (OpenShift)", 2, "2 days ago"),
+    ("na-west-4f84", "OK", "na-west (Podman)", 1, "1/8/2020"),
+    ("headquarters-a96a", "Error", "headquarters (OpenShift)", 1, "3/15/2020"),
+)
+
+link_table = html_table(data, headings=headings, cell_fn=cell)
 
 def cell(column_index, value):
     if column_index == 0:
@@ -33,18 +55,23 @@ def cell(column_index, value):
     return f"<td>{value}</td>"
 
 headings = "Name", "Bindings"
-service_table = html_table_csv("data/services.csv", headings=headings, cell_fn=cell)
 
-props = (
-    ("Name", "na-east"),
-    ("Type", "OpenShift"),
-    ("Status", "OK"),
+data = (
+    ("frontend", 2),
+    ("inventory", 1),
+    ("orders", 1),
+    ("postgres", 1),
+    ("reviews", 1),
+    ("strimzi", 1),
 )
 
-site_properties = html_table(props, class_="properties")
+service_table = html_table(data, headings=headings, cell_fn=cell)
 
-def path_nav(page):
-    separator = "<span class=\"path-separator\">›</span>"
-    links = separator.join(list(page.path_nav_links)[1:])
+props = (
+    ("Name", "na-east-d45e"),
+    ("Status", "OK"),
+    ("Site", "na-east (OpenShift)"),
+    ("Created", "3 minutes ago",),
+)
 
-    return f"<nav class=\"path-nav\">{links}</nav>"
+link_properties = html_table(props, class_="properties")
