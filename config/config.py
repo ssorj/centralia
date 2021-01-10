9e00c6a7d833e2302449eb69877cf2971c80a723
Justin Ross <jross@apache.org>
2021-01-10 07:15:04 -0500

    Initial

---
+++ config/config.py
@@ -0,0 +1,55 @@
+site_url = ""
+
+def cell(column_index, value):
+    if column_index == 0:
+        value = f"<a href=\"/networks/company-co/index.html\">{value}</a>"
+
+    return f"<td>{value}</td>"
+
+headings = "Name", "Status", "Linked sites", "Created"
+network_table = html_table_csv("data/networks.csv", headings=headings, cell_fn=cell)
+
+props = (
+    ("Name", "company-co"),
+    ("Status", "Active"),
+    ("Created", "4 hours ago"),
+)
+
+network_properties = html_table(props, class_="properties")
+
+def cell(column_index, value):
+    if column_index == 0:
+        value = f"<a href=\"/networks/company-co/sites/na-east/index.html\">{value}</a>"
+
+    return f"<td>{value}</td>"
+
+headings = "Name", "Type", "Status"
+site_table = html_table_csv("data/sites.csv", headings=headings, cell_fn=cell)
+
+def cell(column_index, value):
+    if column_index == 0:
+        value = f"<a href=\"/networks/company-co/services/frontend/index.html\">{value}</a>"
+
+    return f"<td>{value}</td>"
+
+headings = "Name", "Bindings"
+service_table = html_table_csv("data/services.csv", headings=headings, cell_fn=cell)
+
+props = (
+    ("Name", "na-east"),
+    ("Type", "OpenShift"),
+    ("Status", "OK"),
+)
+
+site_properties = html_table(props, class_="properties")
+
+def path_nav(page):
+    links = list(page.path_nav_links)
+
+    if len(links) < 2:
+        return ""
+
+    separator = "<span class=\"path-separator\">›</span>"
+    links = separator.join(links[1:])
+
+    return f"<nav id=\"-path-nav\">{links}</nav>"
