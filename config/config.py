e9dfbcf0227804d57666bec57ad133e35477f0eb
Justin Ross <jross@apache.org>
2021-01-13 06:31:39 -0500

    WIP

--- config/config.py
+++ config/config.py
@@ -6,18 +6,26 @@ def path_nav(page):
 
     return f"<nav class=\"path-nav\">{links}</nav>"
 
+def table_link(content, href=""):
+    return f"<a href=\"{href}\">{content}</a>"
+
+def table_button(content, href=""):
+    return f"<a class=\"table-button\" href=\"{href}\">{content}</a>"
+
 def cell(column_index, value):
     if column_index == 0:
-        value = f"<a href=\"/networks/company-co/index.html\">{value}</a>"
+        value = table_link(value, href="/networks/company-co/index.html")
+    elif column_index == 4:
+        value = table_button("Console <span class=\"material-icons-outlined small\">open_in_new</span>")
 
     return f"<td>{value}</td>"
 
-headings = "Name", "Status", "Linked sites", "Created"
+headings = "Name", "Status", "Linked sites", "Created", "Actions"
 
 data = (
-    ("company-co", "Active", "na-east, na-west, and headquarters", "4 hours ago"),
-    ("salty-pug", "Active", "store-1, factory-1, and 4 more", "3 days ago"),
-    ("bookinfo", "Disabled", "productpage, details, and 2 more", "1/8/2020"),
+    ("company-co", "Active", "na-east, na-west, and headquarters", "4 hours ago", None),
+    ("salty-pug", "Active", "store-1, factory-1, and 4 more", "3 days ago", None),
+    ("bookinfo", "Disabled", "productpage, details, and 2 more", "1/8/2020", None),
 )
 
 network_table = html_table(data, headings=headings, cell_fn=cell)
@@ -33,23 +41,27 @@ network_properties = html_table(props, class_="properties")
 
 def cell(column_index, value):
     if column_index == 0:
-        value = f"<a href=\"/networks/company-co/tokens/company-co-497c/index.html\">{value}</a>"
+        value = table_link(value, href="/networks/company-co/tokens/company-co-497c/index.html")
+    elif column_index == 6:
+        value = " ".join((table_button("Download"), table_button("Revoke")))
 
     return f"<td>{value}</td>"
 
-headings = "Name", "Status", "Expires", "Created"
+headings = "Name", "Status", "Use count", "Use limit", "Expires", "Created", "Actions"
 
 data = (
-    ("company-co-497c", "Active", "Never", "3 minutes ago"),
-    ("company-co-af89", "Active", "In 6 hours", "2 days ago"),
-    ("company-co-f8fa", "Revoked", "-", "2 days ago"),
+    ("company-co-497c", "Active", 0, 1, "Never", "3 minutes ago", None),
+    ("company-co-af89", "Active", "-", "-", "In 6 hours", "2 days ago", None),
+    ("company-co-f8fa", "Revoked", "-", "-", "-", "2 days ago", None),
 )
 
 token_table = html_table(data, headings=headings, cell_fn=cell)
 
 props = (
     ("Name", "company-co-497c"),
-    ("Status", "OK"),
+    ("Status", "Active"),
+    ("Use count", 0),
+    ("Use limit", 1),
     ("Expires", "Never"),
     ("Created", "3 minutes ago"),
 )
@@ -58,16 +70,18 @@ token_properties = html_table(props, class_="properties")
 
 def cell(column_index, value):
     if column_index == 0:
-        value = f"<a href=\"/networks/company-co/sites/na-east/index.html\">{value}</a>"
+        value = table_link(value, href="/networks/company-co/sites/na-east/index.html")
+    elif column_index == 5:
+        value = table_button("Unlink")
 
     return f"<td>{value}</td>"
 
-headings = "Name", "Status", "Site type", "Cost", "Linked"
+headings = "Name", "Status", "Site type", "Cost", "Linked", "Actions"
 
 data = (
-    ("na-east", "OK", "OpenShift", 2, "2 days ago"),
-    ("na-west", "OK", "Podman", 1, "1/8/2020"),
-    ("headquarters", "Error: Unreachable", "OpenShift", 1, "3/15/2020"),
+    ("na-east", "OK", "OpenShift", 2, "2 days ago", None),
+    ("na-west", "OK", "Podman", 1, "1/8/2020", None),
+    ("headquarters", "Error: Unreachable", "OpenShift", 1, "3/15/2020", None),
 )
 
 site_table = html_table(data, headings=headings, cell_fn=cell)
@@ -75,36 +89,48 @@ site_table = html_table(data, headings=headings, cell_fn=cell)
 props = (
     ("Name", "na-east"),
     ("Status", "OK"),
+    ("ID", "e8f4c2ca-90cf-4882-9edc-5e9a802a3dab"),
+    ("Version", "1.2.3"),
     ("Site type", "OpenShift"),
+    ("Namespace", "na-east-prod"),
+    ("Cost", 2),
     ("Linked", "2 days ago",),
 )
 
 site_properties = html_table(props, class_="properties")
 
+def cell(column_index, value):
+    if column_index == 3:
+        value = table_button("Create a binding")
+
+    return f"<td>{value}</td>"
+
 headings = "Name", "Replicas", "Bindings", "Actions"
 
 data = (
-    ("frontend", 3, "<a href=\"/networks/company-co/services/frontend/bindings/frontend-2da6/index.html\">frontend-2da6</a>", "<a class=\"table-button\" href=\"\">Add binding</a>"),
-    ("reviews", 2, "-", "<a class=\"table-button\" href=\"\">Add binding</a>"),
-    ("orders", 2, "-", "<a class=\"table-button\" href=\"\">Add binding</a>"),
+    ("frontend", 3, table_link("frontend-2da6", href="/networks/company-co/services/frontend/bindings/frontend-2da6/index.html"), None),
+    ("reviews", 2, "-", None),
+    ("orders", 2, "-", None),
 )
 
-deployment_table = html_table(data, headings=headings)
+deployment_table = html_table(data, headings=headings, cell_fn=cell)
 
 def cell(column_index, value):
     if column_index == 0:
-        value = f"<a href=\"/networks/company-co/services/frontend/index.html\">{value}</a>"
+        value = table_link(value, href="/networks/company-co/services/frontend/index.html")
+    elif column_index == 4:
+        value = table_button("Delete")
 
     return f"<td>{value}</td>"
 
 headings = "Name", "Status", "Bindings", "Created", "Actions"
 
 data = (
-    ("frontend", "OK", 2, "3 minutes ago", "<a class=\"table-button\" href=\"\">Delete</a>"),
-    ("inventory", "OK", 1, "4 hours ago", "<a class=\"table-button\" href=\"\">Delete</a>"),
-    ("orders", "OK", 1, "2 days ago", "<a class=\"table-button\" href=\"\">Delete</a>"),
-    ("database", "OK", 1, "3/15/2020", "<a class=\"table-button\" href=\"\">Delete</a>"),
-    ("reviews", "Error: No bindings", 0, "3/15/2020", "<a class=\"table-button\" href=\"\">Delete</a>"),
+    ("frontend", "OK", 2, "3 minutes ago", None),
+    ("inventory", "OK", 1, "4 hours ago", None),
+    ("orders", "OK", 1, "2 days ago", None),
+    ("database", "OK", 1, "3/15/2020", None),
+    ("reviews", "Error: No bindings", 0, "3/15/2020", None),
 )
 
 service_table = html_table(data, headings=headings, cell_fn=cell)
@@ -119,15 +145,27 @@ service_properties = html_table(props, class_="properties")
 
 def cell(column_index, value):
     if column_index == 0:
-        value = f"<a href=\"/networks/company-co/services/frontend/bindings/frontend-2da6/index.html\">{value}</a>"
+        value = table_link(value, href="/networks/company-co/services/frontend/bindings/frontend-2da6/index.html")
+    elif column_index == 5:
+        value = table_button("Delete")
 
     return f"<td>{value}</td>"
 
 headings = "Name", "Status", "Target", "Site", "Created", "Actions"
 
 data = (
-    ("frontend-2da6", "OK", "deployment/frontend", "na-east (OpenShift)", "2 days ago", "<a class=\"table-button\" href=\"\">Delete</a>"),
-    ("frontend-45cc", "Error: Unreachable", "container/frontend", "na-west (Podman)", "1 day ago", "<a class=\"table-button\" href=\"\">Delete</a>"),
+    ("frontend-2da6", "OK", "deployment/frontend", "na-east (OpenShift)", "2 days ago", None),
+    ("frontend-45cc", "Error: Unreachable", "container/frontend", "na-west (Podman)", "1 day ago", None),
 )
 
 binding_table = html_table(data, headings=headings, cell_fn=cell)
+
+props = (
+    ("Name", "frontend-2da6"),
+    ("Status", "OK"),
+    ("Target", "deployment/frontend"),
+    ("Site", table_link("na-east (OpenShift)", href="/networks/company-co/sites/na-east/index.html")),
+    ("Created", "2 days ago",),
+)
+
+binding_properties = html_table(props, class_="properties")
--- input/main.js
+++ input/main.js
@@ -40,31 +40,45 @@ window.addEventListener("load", () => {
     }
 });
 
+function updateTabs(tabs, href) {
+    let url = new URL(href);
+    let selectedTabId = url.searchParams.get("tab");
+
+    if (!selectedTabId) {
+        tabs.$(":scope > nav > a").classList.add("selected");
+        tabs.$(":scope > div").style.display = "inherit";
+        return;
+    }
+
+    for (let link of tabs.$$(":scope > nav > a")) {
+        if (link.href == href) {
+            link.classList.add("selected");
+        } else {
+            link.classList.remove("selected");
+        }
+    }
+
+    for (let pane of tabs.$$(":scope > div")) {
+        if (pane.id == selectedTabId) {
+            pane.style.display = "inherit";
+        } else {
+            pane.style.display = "none";
+        }
+    }
+}
+
+window.addEventListener("load", () => {
+    for (let tabs of $$(".tabs")) {
+        updateTabs(tabs, window.location.href);
+    }
+});
+
 for (let tabs of $$(".tabs")) {
     for (let link of tabs.$$(":scope > nav > a")) {
         link.addEventListener("click", (event) => {
             event.preventDefault();
-
-            let selectedId = new URL(link.href).hash.substr(1)
-
-            for (let ilink of tabs.$$(":scope > nav > a")) {
-                if (ilink.href == link.href) {
-                    ilink.classList.add("selected");
-                } else {
-                    ilink.classList.remove("selected");
-                }
-            }
-
-            for (let pane of tabs.$$(":scope > div")) {
-                if (pane.id == selectedId) {
-                    pane.style.display = "inherit";
-                } else {
-                    pane.style.display = "none";
-                }
-            }
+            window.history.replaceState(null, null, link.href);
+            updateTabs(tabs, link.href);
         });
     }
-
-    tabs.$(":scope > nav > a").classList.add("selected");
-    tabs.$(":scope > div").style.display = "inherit";
 }
